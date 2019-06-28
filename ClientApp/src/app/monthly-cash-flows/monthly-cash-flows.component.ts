da46ed0dc6d13b058e81e5f7a3c2c1e0cd2d8c2c
James Byrd <James.Byrd1265@gmail.com>
2019-06-27 23:58:02 -0400

    feat: calc monthly cash flows

--- ClientApp/src/app/monthly-cash-flows/monthly-cash-flows.component.ts
+++ ClientApp/src/app/monthly-cash-flows/monthly-cash-flows.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, ngOnInit, Input } from '@angular/core';
+﻿import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'monthly-cash-flows',
@@ -7,8 +7,46 @@
 })
 
 export class MonthlyCashFlowsComponent {
-  @Input() loans: Array<{month: number, interest: number, principal: number, balance: number}>;
+  @Input() loans: Array<{ month: number, balance: number, term: number, rate: number }>;
+  monthlyCashFlows: Array<MonthlyCashFlow> = [];
+  totalMonthlyPayment: number;
+  interest: number;
+  principal: number;
+  monthlyCashFlowData: MonthlyCashFlow;
+
     ngOnInit() {
-      console.log('loans:  ', this.loans)
-    }
+
+        this.calcCashFlow()
+                console.log(this.monthlyCashFlows)
+  }
+
+  calcCashFlow() {
+      this.loans.forEach(({term, rate, balance}, idx) => {
+          while (term > 0) {
+              idx += 1
+              this.totalMonthlyPayment = (balance * (rate / 1200)) / +((1 - (1 + rate / 1200) ** (-1 * term)));
+              this.interest = +(balance * +(rate / 1200)).toFixed(2);
+              this.principal = +(this.totalMonthlyPayment - this.interest).toFixed(2);
+              balance = balance - this.principal;
+              this.monthlyCashFlowData = {
+                  month: idx,
+                  interest: this.interest,
+                  principal: this.principal,
+                  balance: balance
+              }
+              this.monthlyCashFlows.push(this.monthlyCashFlowData);
+              term -= 1
+          }
+
+
+      })
+     
+  }
+}
+
+interface MonthlyCashFlow {
+    month: number,
+    interest: number,
+    principal: number,
+    balance: number
 }
