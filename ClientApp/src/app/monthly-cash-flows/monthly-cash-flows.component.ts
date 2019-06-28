22e83c6fdcba3626dc6fae35bd257c8f6b156788
James Byrd <James.Byrd1265@gmail.com>
2019-06-28 15:41:03 -0400

    style: display relevant components

--- ClientApp/src/app/monthly-cash-flows/monthly-cash-flows.component.ts
+++ ClientApp/src/app/monthly-cash-flows/monthly-cash-flows.component.ts
@@ -22,24 +22,24 @@ export class MonthlyCashFlowsComponent {
   }
 
   calcCashFlow() {
-      let currentMonth = 1
-      this.remainingBalance = this.currentLoan.balance;
-      this.loanTerm = this.currentLoan.term;
-      while (this.loanTerm > 0 || this.remainingBalance) {
-        this.totalMonthlyPayment = (this.remainingBalance * (this.currentLoan.rate / 1200)) / +((1 - (1 + this.currentLoan.rate / 1200) ** (-1 * this.loanTerm)));
-        this.interest = +(this.remainingBalance * +(this.currentLoan.rate / 1200)).toFixed(2);
-        this.principal = +(this.totalMonthlyPayment - this.interest).toFixed(2);
-        this.remainingBalance = +(this.remainingBalance - this.principal).toFixed(2);
-        this.monthlyCashFlowData = {
-            month: currentMonth,
-            interest: this.interest,
-            principal: this.principal,
-            balance: this.remainingBalance
-        }
-        this.monthlyCashFlows.push(this.monthlyCashFlowData);
-        this.loanTerm -= 1
-        currentMonth += 1;
-      }    
+    let currentMonth = 1
+    this.remainingBalance = this.currentLoan.balance;
+    this.loanTerm = this.currentLoan.term;
+    while (this.loanTerm > 0 || this.remainingBalance) {
+      this.totalMonthlyPayment = (this.remainingBalance * (this.currentLoan.rate / 1200)) / +((1 - (1 + this.currentLoan.rate / 1200) ** (-1 * this.loanTerm)));
+      this.interest = +(this.remainingBalance * +(this.currentLoan.rate / 1200)).toFixed(2);
+      this.principal = +(this.totalMonthlyPayment - this.interest).toFixed(2);
+      this.remainingBalance = +(this.remainingBalance - this.principal).toFixed(2);
+      this.monthlyCashFlowData = {
+          month: currentMonth,
+          interest: this.interest,
+          principal: this.principal,
+          balance: this.remainingBalance
+      }
+      this.monthlyCashFlows.push(this.monthlyCashFlowData);
+      this.loanTerm -= 1
+      currentMonth += 1;
+    }    
   }
 }
 
